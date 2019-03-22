a399c7b02a927653dd7ad3ac628fc7038a5b0eca
TheMiguel <miguel.alv.gomes@gmail.com>
2019-03-22 10:50:20 +0100

    core functionality implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,7 +49,7 @@ export class AppComponent implements OnInit {
       new ComputerScienceModules("Web Information Retrievel", "SS", 6),
       new ComputerScienceModules("Weiterführung Stochastik", "", 9),
       new ComputerScienceModules("Spezial Topics in Data Analytics", "", 9),
-      new ComputerScienceModules("Angewante Statistik", "SS", 9),
+      new ComputerScienceModules("Angewandte Statistik", "SS", 9),
       new ComputerScienceModules("Wahrscheinlichkeitstheorie", "WS", 9),
       new ComputerScienceModules("Big Data Technologien", "WS", 6),
       new ComputerScienceModules("Seminar Data Analytics", "SS", 3)
@@ -122,28 +122,53 @@ export class AppComponent implements OnInit {
     return value;
   }
 
+  calculateMarkModule(moduleList: ComputerScienceModules[]): number {
+    let value = 0;
+    let weight = 0;
+    for (const m of moduleList) {
+      value += m.isSelected ? m.mark * m.cp : 0;
+      weight += m.isSelected && m.mark > 0 ? m.cp : 0;
+    }
+    return weight > 0 ? Number((value / weight).toFixed(4)) : 0;
+  }
+
   calculateWPAllModules(): number {
     let value = 0;
-    for (const i of this.moduleCoreTopics) {
-      value += i.isSelected ? i.cp : 0;
-    }
-    for (const i of this.moduleDataAnalytics) {
-      value += i.isSelected ? i.cp : 0;
-    }
-    for (const i of this.moduleScientificComputing) {
-      value += i.isSelected ? i.cp : 0;
-    }
-    for (const i of this.moduleComputerEngineering) {
-      value += i.isSelected ? i.cp : 0;
-    }
-    for (const i of this.moduleInterdisiplinary) {
-      value += i.isSelected ? i.cp : 0;
-    }
-    for (const i of this.moduleAdditional) {
-      value += i.isSelected ? i.cp : 0;
-    }
-      value += this.moduleMasterThesis.isSelected ? this.moduleMasterThesis.cp : 0;
+    value += this.calculateWPModule(this.moduleCoreTopics);
+    value += this.calculateWPModule(this.moduleDataAnalytics);
+    value += this.calculateWPModule(this.moduleScientificComputing);
+    value += this.calculateWPModule(this.moduleComputerEngineering);
+    value += this.calculateWPModule(this.moduleInterdisiplinary);
+    value += this.calculateWPModule(this.moduleAdditional);
+    value += this.calculateWPModule([this.moduleMasterThesis]);
     return value;
+  }
+
+  calculateMarkAllModules(): number {
+    let value = 0;
+    let weigth = 0;
+    value += this.calculateMarkModule(this.moduleCoreTopics) * this.calculateWPModule(this.moduleCoreTopics);
+    weigth += this.calculateMarkModule(this.moduleCoreTopics) > 0 ? this.calculateWPModule(this.moduleCoreTopics) : 0;
+
+    value += this.calculateMarkModule(this.moduleDataAnalytics) * this.calculateWPModule(this.moduleDataAnalytics);
+    weigth += this.calculateMarkModule(this.moduleDataAnalytics) > 0 ? this.calculateWPModule(this.moduleDataAnalytics) : 0;
+
+    value += this.calculateMarkModule(this.moduleScientificComputing) * this.calculateWPModule(this.moduleScientificComputing);
+    weigth += this.calculateMarkModule(this.moduleScientificComputing) > 0 ? this.calculateWPModule(this.moduleScientificComputing) : 0;
+
+    value += this.calculateMarkModule(this.moduleComputerEngineering) * this.calculateWPModule(this.moduleComputerEngineering);
+    weigth += this.calculateMarkModule(this.moduleComputerEngineering) > 0 ? this.calculateWPModule(this.moduleComputerEngineering) : 0;
+
+    value += this.calculateMarkModule(this.moduleInterdisiplinary) * this.calculateWPModule(this.moduleInterdisiplinary);
+    weigth += this.calculateMarkModule(this.moduleInterdisiplinary) > 0 ? this.calculateWPModule(this.moduleInterdisiplinary) : 0;
+
+    value += this.calculateMarkModule(this.moduleAdditional) * this.calculateWPModule(this.moduleAdditional);
+    weigth += this.calculateMarkModule(this.moduleAdditional) > 0 ? this.calculateWPModule(this.moduleAdditional) : 0;
+
+    value += this.calculateMarkModule([this.moduleMasterThesis]) * this.calculateWPModule([this.moduleMasterThesis]);
+    weigth += this.calculateMarkModule([this.moduleMasterThesis]) > 0 ? this.calculateWPModule([this.moduleMasterThesis]) : 0;
+
+    return weigth > 0 ? Number((value / weigth).toFixed(4)) : 0;
   }
 
   toLessWPDataAnalytics(): boolean {
@@ -153,8 +178,15 @@ export class AppComponent implements OnInit {
     this.moduleDataAnalytics.forEach(m => value += m.isSelected ? m.cp : 0);
     this.moduleScientificComputing.forEach(m => value2 += m.isSelected ? m.cp : 0);
     this.moduleComputerEngineering.forEach(m => value3 += m.isSelected ? m.cp : 0);
-    return this._mainFocus === 0 ? value >= 39 : 
-      value + (this._mainFocus === 1 ? value2 : value3) > 18;
+    if(this._mainFocus === 0) {
+      return value < 39;
+    }else if(this._mainFocus === 1) {
+      return (value + value3) < 18;
+    }else if(this._mainFocus === 2) {
+      return (value + value2) < 18;
+    }else {
+      return (value + value2 + value3) <18;
+    }
   }
   toLessWPScientificComputing(): boolean {
     let value = 0;
@@ -163,8 +195,15 @@ export class AppComponent implements OnInit {
     this.moduleDataAnalytics.forEach(m => value += m.isSelected ? m.cp : 0);
     this.moduleScientificComputing.forEach(m => value2 += m.isSelected ? m.cp : 0);
     this.moduleComputerEngineering.forEach(m => value3 += m.isSelected ? m.cp : 0);
-    return this._mainFocus === 1 ? value2 >= 39 : 
-      value2 + (this._mainFocus === 0 ? value : value3) > 18;
+    if(this._mainFocus === 0) {
+      return (value2 + value3) < 18;
+    }else if(this._mainFocus === 1) {
+      return value2 < 39;
+    }else if(this._mainFocus === 2) {
+      return (value + value2) < 18;
+    }else {
+      return (value + value2 + value3) <18;
+    }
   }
   toLessWPComputerEngineering(): boolean {
     let value = 0;
@@ -173,29 +212,36 @@ export class AppComponent implements OnInit {
     this.moduleDataAnalytics.forEach(m => value += m.isSelected ? m.cp : 0);
     this.moduleScientificComputing.forEach(m => value2 += m.isSelected ? m.cp : 0);
     this.moduleComputerEngineering.forEach(m => value3 += m.isSelected ? m.cp : 0);
-    return this._mainFocus === 2 ? value3 >= 39 : 
-      value3 + (this._mainFocus === 1 ? value2 : value) > 18;
+    if(this._mainFocus === 0) {
+      return (value2 + value3) < 18;
+    }else if(this._mainFocus === 1) {
+      return (value + value3) < 18;
+    }else if(this._mainFocus === 2) {
+      return value3 < 39;
+    }else {
+      return (value + value2 + value3) <18;
+    }
   }
   toLessWPInterdisiplinary(): boolean {
     let value = 0;
     this.moduleInterdisiplinary.forEach(m => value += m.isSelected ? m.cp : 0);
-    return value >= 9;
+    return value < 9 || value > 18;
   }
   toLessWPAdditional(): boolean {
     let value = 0;
     this.moduleAdditional.forEach(m => value += m.isSelected ? m.cp : 0);
-    return value >= 6;
+    return value < 6 || value > 15;
   }
 
   isSeminarSelected(main: MainFocus): boolean {
     let selected = false;
-    if (main = MainFocus.dataAnalytics) {
+    if (main === MainFocus.dataAnalytics) {
       this.moduleDataAnalytics.forEach(m => selected = selected ? true : m.isSelected && m.cp ===3);
     }
-    else if(main = MainFocus.scientificComputing) {
+    else if(main === MainFocus.scientificComputing) {
       this.moduleScientificComputing.forEach(m => selected = selected ? true : m.isSelected && m.cp ===3);
     }
-    else {
+    else if(main === MainFocus.computerEngineering){
       this.moduleComputerEngineering.forEach(m => selected = selected ? true : m.isSelected && m.cp ===3);
     }
     return selected;
